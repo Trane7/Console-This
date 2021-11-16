dac6c8d60b11d059aaef365ac0c788b25a0f8a52
Ryan Lopez <seek916@gmail.com>
2021-11-15 20:19:06 -0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -25,27 +25,3 @@ var fetchConsoles = function(selectConsole, zipCode) {
   console.log(selectConsole);
   console.log(zipcode);
 }
-=======
-     const APIKey = "XMAvBzSYr4Q7yiRb5DYdO6b4";
-
-     function selectConsole(val) {
-     //if console is in array
-     if (selectedConsoleArray.includes(val)) {
-     var x = document.getElementById(val);
-     x.style = "background-color: #A333C8 !important"; 
-     var index = selectedConsoleArray.indexOf(val);
-     selectedConsoleArray.splice(index, 1);
-     console.log(selectedConsoleArray);
-     } else {
-     //if console is not in array
-     var x = document.getElementById(val);
-     x.style = "background-color: black !important";
-     selectedConsoleArray.push(val);
-     console.log(selectedConsoleArray);
-  }
-}
-/*var bby = require('bestbuy')(APIKey);
-bby.stores('postalCode=55423', {show: 'storeId,storeType,name,city,region'}).then(function(data){
-  console.log(data);
-});*/
->>>>>>> 47ea07fbcda521f7c6bd0feb90d7ab791e08ff57
